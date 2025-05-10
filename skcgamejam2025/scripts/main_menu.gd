94f0f565c9d5d373eecc43ee89debeee80b2f3b4
Stevan <evastnicle.stev@gmail.com>
2025-05-10 22:52:29 +0200

    Add menu and credits screen.

---
+++ skcgamejam2025/scripts/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
