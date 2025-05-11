ac993348f7d6c250fcad1ae8b683b103145f2413
Stevan <evastnicle.stev@gmail.com>
2025-05-11 15:40:49 +0200

    Add final level.

---
+++ skcgamejam2025/level_5.gd
@@ -0,0 +1,12 @@
+extends BaseLevel
+
+func _ready() -> void:
+	initiate_turn_manager()
+	get_used_rect_for_layer($TileMapLayer)
+	Globals.max_bullets = 3
+	Globals.current_bullets = 3
+	Globals.max_evil_sofas = 10
+	Globals.current_evil_sofas = 10
+	$UI.clear_bullet_sprites()
+	$UI.init_bullet_sprites()
+	$UI.init_evil_sofa_sprites()
