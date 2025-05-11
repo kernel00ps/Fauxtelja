a6241bc6879ad962ff9803262e2c2f3357960d9a
kernel00ps <davidanderson27@protonmail.com>
2025-05-11 16:57:36 +0200

    main menu image and some instructions menu bits

--- skcgamejam2025/scripts/main_menu.gd
+++ skcgamejam2025/scripts/main_menu.gd
@@ -8,3 +8,6 @@ func _on_credits_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_instructions_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/instructions_menu.tscn")
