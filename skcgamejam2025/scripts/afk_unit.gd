1201501affe013756a3acde08919008441e6581a
Stevan <evastnicle.stev@gmail.com>
2025-05-11 12:22:17 +0200

    Add afk units.

---
+++ skcgamejam2025/scripts/afk_unit.gd
@@ -0,0 +1,15 @@
+extends EnemyUnit
+class_name AfkUnit
+
+func start_turn():
+	can_act = true;
+	end_turn();
+	return
+
+func die() -> void:
+	if TurnManager.enemy_units.has(self):
+		var was_my_turn = (TurnManager.enemy_units[TurnManager.current_unit_index] == self)
+		TurnManager.enemy_units.erase(self)
+		queue_free()
+		if was_my_turn:
+			TurnManager.unit_finished_turn()
