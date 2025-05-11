fc6934f1a248156518968e1e604287fa83d1dc46
Stevan <evastnicle.stev@gmail.com>
2025-05-11 15:31:41 +0200

    Add levels.

--- skcgamejam2025/levels/level_4.gd
+++ skcgamejam2025/levels/level_4.gd
@@ -1,13 +1,12 @@
 extends BaseLevel
 
-
 func _ready() -> void:
 	initiate_turn_manager()
 	get_used_rect_for_layer($TileMapLayer)
-	Globals.max_bullets = 5
-	Globals.current_bullets = 5
-	Globals.max_evil_sofas = 5
-	Globals.current_evil_sofas = 5
+	Globals.max_bullets = 2
+	Globals.current_bullets = 2
+	Globals.max_evil_sofas = 4
+	Globals.current_evil_sofas = 4
 	$UI.clear_bullet_sprites()
 	$UI.init_bullet_sprites()
 	$UI.init_evil_sofa_sprites()
